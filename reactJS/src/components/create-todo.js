c2a2da69f26ffd5efa1df7cabbd527e05c953db2
Nattie87 <natalie.huitson@gmail.com>
2017-05-13 16:58:53 +0100

    finishing off the task page

---
+++ reactJS/src/components/create-todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TodosList extends React.Component {
+  render() {
+    return (
+        <form>
+        <input type="text" placeholder="What do i need todo"/>
+        <button>Create</button>
+        </form>
+    );
+  }
+}
--- reactJS/src/components/todos-list-item.js
+++ reactJS/src/components/todos-list-item.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 
 export default class TodosListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    };
+  }
+
+  renderActionsSection() {
+    if (this.state.isEditing) {
+      return (
+        <button onClick={this.onEditClick.bind(this)}>Save</button>
+        <button>Cancel</button>
+      );
+    }
+    return (
+      <button onClick={this.onEditClick.bind(this)}>Edit</button>
+      <button>Delete</button>
+    );
+  }
   render() {
     return (
       <tr>
-          <td>{this.props.task}</td>
-          <td>
-          <button>Edit</button>
-          <button>Delete</button>
-          </td>
+        <td>{this.props.task}</td>
+        {this.renderActionSection()}
       </tr>
     );
+  }
+  onEditClick() {
+    this.setState({ isEditing: true });
   }
 }
