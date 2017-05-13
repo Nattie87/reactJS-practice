aed445c47308bcc426cf23ae5e6492dd217b506e
Nattie87 <natalie.huitson@gmail.com>
2017-05-13 12:36:55 +0100

    page is rendering thank god

--- reactJS/src/components/app.js
+++ reactJS/src/components/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-  export default App extends React.Components {
-    render() {
-      return (
-        <div>
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
         <h1>React ToDos App</h1>
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
