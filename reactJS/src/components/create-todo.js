6a2432e5db2256f1c0d6714d52c3045a2eb1cc46
Nattie87 <natalie.huitson@gmail.com>
2017-05-14 14:38:06 +0100

    createTask

--- reactJS/src/components/create-todo.js
+++ reactJS/src/components/create-todo.js
@@ -3,10 +3,14 @@ import React from 'react';
 export default class TodosList extends React.Component {
   render() {
     return (
-        <form>
-        <input type="text" placeholder="What do i need todo"/>
+        <form onSubmit={this.handleCreate.bind(this)}>
+        <input type="text" placeholder="What do i need todo" ref="createInput"/>
         <button>Create</button>
         </form>
     );
+  }
+  handleCreate(event) {
+    event.preventDefault();
+    this.props.createTask(this.refs.createInput.value);
   }
 }
