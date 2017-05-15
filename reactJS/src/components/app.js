4fa0ed2a8d2dae855d80397f973e4fa293372535
Nattie87 <natalie.huitson@gmail.com>
2017-05-15 12:06:16 +0100

    adding changes

--- reactJS/src/components/app.js
+++ reactJS/src/components/app.js
@@ -4,30 +4,29 @@ import TodosList from './todos-list';
 
 const todos = [
   {
-    task: 'Make React video',
+    task: 'make React tutorial',
     isCompleted: false
   },
   {
-    task: 'Eat Lunch',
+    task: 'eat dinner',
     isCompleted: true
   }
 ];
 
 export default class App extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        todos
-      };
-    }
+    this.state = {
+      todos
+    };
+  }
   render() {
-    var inlineStyleH1 = {color: "yellow"};
     return (
+      <tr>
+      <h1>React ToDos App</h1>
       <div>
-        <h1 style={inlineStyleH1}>React ToDos App</h1>
-        <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)}
- />
+        <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)} />
         <TodosList
           todos={this.state.todos}
           toggleTask={this.toggleTask.bind(this)}
@@ -35,15 +34,16 @@ export default class App extends React.Component {
           deleteTask={this.deleteTask.bind(this)}
           />
       </div>
+      </tr>
     );
   }
 
-  toggleTask(task){
+  toggleTask(task) {
     const foundTodo = _.find(this.state.todos, todo => todo.task === task);
     foundTodo.isCompleted = !foundTodo.isCompleted;
     this.setState({ todos: this.state.todos });
-
   }
+
   createTask(task) {
     this.state.todos.push({
       task,
@@ -53,16 +53,13 @@ export default class App extends React.Component {
   }
 
   saveTask(oldTask, newTask) {
-    const foundTodo =  _.find(this.state.todos, todo => todo.task === oldTask);
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
     foundTodo.task = newTask;
     this.setState({ todos: this.state.todos });
   }
 
-  deleteTask(taskToDelete){
+  deleteTask(taskToDelete) {
     _.remove(this.state.todos, todo => todo.task === taskToDelete);
     this.setState({ todos: this.state.todos });
   }
 }
-
-
-// ._ are lodash methods. the dependency i installed earlier. like _.find
